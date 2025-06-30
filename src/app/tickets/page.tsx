59f4b5831e819e4dcbfa9954430a7c811d25047f
Mark Randall <lyndsayrandall@me.com>
2025-06-30 19:58:48 +1000

    Checking for verecel

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,7 +6,7 @@ import TicketItem from "@/features/components/ticket-item";
 const TicketsPage = () =>{
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <Heading title ="Tickets" description="All Your tickets in one place"/>
+      <Heading title ="Tickets" description=" Displaying all Your tickets in one place"/>
       <div className= "flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
           <TicketItem key = {ticket.id} ticket={ticket} />       
