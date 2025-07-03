e1563193ee73c9bbadf3a7673c2a062052ce78a3
Mark Randall <lyndsayrandall@me.com>
2025-07-03 14:13:56 +1000

    removed lint error

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -3,7 +3,7 @@
 import { notFound } from "next/navigation";
 import TicketItem from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
-import { ticketsPath } from "@/paths";
+
 
 
 
