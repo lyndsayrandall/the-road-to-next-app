69a449860cc053da6874b7f8ba53d938bd9216d4
Mark Randall <lyndsayrandall@me.com>
2025-07-19 13:57:38 +1000

    Fixed lint errors to cookies

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -2,16 +2,14 @@
 
 import { Ticket } from "@prisma/client";
 import { useActionState} from "react";
-import { toast} from "sonner";
+import { Form } from "@/components/form";
 import { FieldError } from "@/components/form/field-error";
-import { useActionFeedBackHook } from "@/components/form/hooks/use-action-feedback";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { upsertTicket } from "../actions/upsert-ticket";
-import { Form } from "@/components/form";
 
 
 
