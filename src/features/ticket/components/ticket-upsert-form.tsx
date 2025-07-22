a9d7bd3923857326f1674498925560f8339c0943
Mark Randall <lyndsayrandall@me.com>
2025-07-22 11:19:43 +1000

    Advanced UIs lint fix only

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -2,6 +2,7 @@
 
 import { Ticket } from "@prisma/client";
 import { useActionState} from "react";
+import { DatePicker } from "@/components/date-picker";
 import { Form } from "@/components/form";
 import { FieldError } from "@/components/form/field-error";
 import { SubmitButton } from "@/components/form/submit-button";
@@ -11,7 +12,6 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { fromCent } from "@/utils/currency";
 import { upsertTicket } from "../actions/upsert-ticket";
-import { DatePicker } from "@/components/date-picker";
 
 
 
@@ -47,20 +47,13 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
             <div className="flex gap-x-2 mb-10">
                 <div className="w-1/2">
                     <Label htmlFor="deadline">Deadline</Label>
-                    {/* <Input  id="deadline" 
-                            name="deadline" 
-                            type= "date"
-                            defaultValue = {
-                                (actionState.payload?.get("deadline") as string) ?? ticket?.deadline
-                            }></Input> */}
                     <DatePicker 
                         id="deadline"
                         name="deadline"
                         defaultValue={
                             (actionState.payload?.get("deadline") as string) ?? 
                             ticket?.deadline
-                        }
-                             
+                        }      
                     />
                     <FieldError actionState={actionState} name="deadline" />
                 </div> 
